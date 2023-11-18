6ab75a1eef6d751e3d4fb0fc891c1a0735835db4
camilozv21 <camilozv21@gmail.com>
2023-11-18 13:58:26 -0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import FormLineal from "../components/FormLineal"
+import { TableMovements } from "../components/TableMovements"
 
 export const Home = () => {
   return (
     <>
       <FormLineal />
+      <TableMovements />
     </>
   )
 }
