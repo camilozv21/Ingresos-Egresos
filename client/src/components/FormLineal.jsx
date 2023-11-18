ef10c27fb045b821a36a6ebac4bb231a3b5f1c9e
camilozv21 <camilozv21@gmail.com>
2023-11-18 17:01:02 -0500

    Update FormLineal.jsx

--- client/src/components/FormLineal.jsx
+++ client/src/components/FormLineal.jsx
@@ -32,18 +32,18 @@ function FormLineal() {
 
   return (
     <form className="w-full" onSubmit={e => handleSubmit(e)}>
-      <div className="flex flex-col sm:flex-row sm:justify-around items-center sm:flex-nowrap border-b border-teal-500 py-2 gap-2">
+      <div className="flex flex-row justify-between sm:justify-around items-center sm:flex-nowrap border-b border-teal-500 py-2 gap-2">
         <input
           type="date"
           name="date"
           value={date}
-          className="w-28 md:w-36"
+          className="w-14 md:w-36"
           onChange={e => setDate(e.target.value)}
         />
         <select
           name="typeAmount"
           value={typeAmount}
-          className="w-28 md:w-32"
+          className="w-14 md:w-32"
           onChange={e => setTypeAmount(e.target.value)}
         >
           <option value="">Tipo de Monto</option>
@@ -55,16 +55,16 @@ function FormLineal() {
           name="amount"
           placeholder='Monto'
           value={amount}
-          className="w-20 md:w-24 placeholder-black"
+          className="w-14 md:w-24 placeholder-black"
           onChange={e => setAmount(e.target.value)}
         />
         <select
           name="category"
           value={category}
-          className="w-28 md:w-24"
+          className="w-14 md:w-24"
           onChange={e => setCategory(e.target.value)}
         >
-          <option value="">Categoría</option>
+          <option value="">Categoría</option>x
           <option value="Transporte">Transporte</option>
           <option value="Comida">Comida</option>
           <option value="Cuota Mensual Padres">Cuota Mensual Padres</option>
@@ -75,7 +75,7 @@ function FormLineal() {
           name="description"
           value={description}
           placeholder='Descripción'
-          className="w-28 md:w-24 h-6  placeholder-black"
+          className="w-14 md:w-24 h-6  placeholder-black"
           onChange={e => setDescription(e.target.value)}
         />
         <button
