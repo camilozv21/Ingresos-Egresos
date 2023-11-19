5338e07769e1e5ea7d9db3621640fe777e4773b5
camilozv21 <camilozv21@gmail.com>
2023-11-18 21:57:03 -0500

    Update FormLineal.jsx

--- client/src/components/FormLineal.jsx
+++ client/src/components/FormLineal.jsx
@@ -82,7 +82,7 @@ function FormLineal() {
           className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
           type="submit"
         >
-          Submit
+          Agregar
         </button>
       </div>
     </form>
