6d24f4580e4133fd643be16fc8ba337d3c36de8d
camilozv21 <camilozv21@gmail.com>
2023-11-18 21:57:19 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import { NavigationBar } from "./components/NavigationBar";
 import { Home } from "./pages/Home";
+import { Charts } from "./components/Charts";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <NavigationBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/charts" element={<Charts />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
